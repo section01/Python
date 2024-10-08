a4ee1a9d2964259b4b8cdc64d798748fee57d7e2
section01 <section01.act@gmail.com>
2024-10-08 18:48:51 +0900

    モック作成

--- apps/app.py
+++ apps/app.py
@@ -27,7 +27,37 @@ engine = SQLAlchemy(app)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('kintai-input.html')
+
+@app.route('/kintai/input')
+def input():
+    return render_template('kintai-input.html')
+
+@app.route('/kintai/search')
+def search():
+    return render_template('kintai-search.html', list=[
+        {
+            "date": "01",
+            "start": "09:00",
+            "end": "18:00",
+            "break": "1:00",
+            "remark": "テスト1"
+        },
+        {
+            "date": "02",
+            "start": "09:00",
+            "end": "18:00",
+            "break": "1:00",
+            "remark": "テスト2"
+        },
+        {
+            "date": "03",
+            "start": "09:00",
+            "end": "18:00",
+            "break": "1:00",
+            "remark": "テスト3"
+        }
+    ])
 
 if __name__ == '__main__':
     app.run(debug=True)
